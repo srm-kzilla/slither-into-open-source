c3f38c1a932c1005851167b7f53e8d11b849e6bc
Utkarsh575 <35625228+Utkarsh575@users.noreply.github.com>
2022-10-21 18:01:55 +0530

    :sparkles: adds vowelCheck.cpp

---
+++ Level-I/vowelCheck.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char c;
+
+    cout<<"enter a character :- "<<endl;
+    cin>>c;
+
+    switch (case)
+    {
+    case 'a'
+        cout<<"it's a vowel "<<endl;
+        break;
+    case 'e'
+        cout<<"it's a vowel "<<endl;
+        break;
+    case 'i'
+        cout<<"it's a vowel "<<endl;
+        break;
+    case 'o'
+        cout<<"it's a vowel "<<endl;
+        break;
+    case 'u'
+        cout<<"it's a vowel "<<endl;
+        break;          
+    default:
+        cout<<"it's a consonant"<<endl;
+
+        break;
+    }
+
+
+    return 0;
+}
