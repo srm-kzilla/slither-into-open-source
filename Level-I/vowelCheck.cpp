488b7b999651d3700347c5dce9e0013962175d7c
Balaram <balaramreddy1710@gmail.com>
2022-10-30 13:35:19 +0530

    Fixing errors

--- Level-I/vowelCheck.cpp
+++ Level-I/vowelCheck.cpp
@@ -1,36 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     char c;
 
-    cout<<"enter a character :- "<<endl;
-    cin>>c;
+    cout << "enter a character :- " << endl;
+    cin >> c;
 
-    switch (case)
+    switch (c)
     {
-    case 'a'
-        cout<<"it's a vowel "<<endl;
+    case 'a':
+        cout << "it's a vowel " << endl;
         break;
-    case 'e'
-        cout<<"it's a vowel "<<endl;
+    case 'e':
+        cout << "it's a vowel " << endl;
         break;
-    case 'i'
-        cout<<"it's a vowel "<<endl;
+    case 'i':
+        cout << "it's a vowel " << endl;
         break;
-    case 'o'
-        cout<<"it's a vowel "<<endl;
+    case 'o':
+        cout << "it's a vowel " << endl;
         break;
-    case 'u'
-        cout<<"it's a vowel "<<endl;
-        break;          
+    case 'u':
+        cout << "it's a vowel " << endl;
+        break;
     default:
-        cout<<"it's a consonant"<<endl;
-
+        cout << "it's a consonant" << endl;
         break;
     }
-
 
     return 0;
 }
